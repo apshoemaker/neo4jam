b3317e6de46f076ecbfff8bf115b27816b5f001c
Paul Shoemaker <apshoemaker@Pauls-MBP.hsd1.tn.comcast.net>
2015-03-23 08:18:29 -0500

    refactored to make it easier to add new jams in the future

--- neo4jam/src/main/java/com/ideacandi/Neo4Jam.java
+++ neo4jam/src/main/java/com/ideacandi/Neo4Jam.java
@@ -1,6 +1,7 @@
 package com.ideacandi;
 
 import org.apache.log4j.Logger;
+import com.ideacandi.jams.BarabasiAlbert;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
@@ -15,222 +16,46 @@ import org.neo4j.graphdb.index.Index;
 import org.neo4j.tooling.GlobalGraphOperations;
 import org.neo4j.helpers.collection.IteratorUtil;
 
-import java.util.Set;
-import java.util.List;
-import java.util.HashMap;
-import java.util.Random;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
 public class Neo4Jam {
-    private static enum RelTypes implements RelationshipType {
-        CONNECTED
-    }
     public static final Logger logger = Logger.getLogger("Neo4Jams");
+
     private GraphDatabaseService _graphdb;
 
+    /**
+     * let's get this party started
+     */
     public static void main(final String[] args){
-        String graphFile = null;
-        if(args.length > 0){
-            graphFile = args[0];
-        }
+        if(args.length != 2){
+            logger.error("Usage:  neo4jam <algo> <graph_file>");
+        } else {
+            String algo = args[0];
+            String graphFile = args[1];
 
-        Neo4Jam jam = new Neo4Jam(graphFile);
-        jam.generateBarabasiAlbert(1000, 3);
+            switch(algo){
+                case "ba":
+                    logger.info("Barabasi Albert warming up ...");
+                    GraphDatabaseService graphdb = embedGraphDb(graphFile);
+                    BarabasiAlbert ba = new BarabasiAlbert();
+                    ba.godo(graphdb);
+            }
+        }
     }
-
-    public Neo4Jam(String graphFile){
+    
+    /**
+     * create a new database or embed from an existing path
+     */
+    public static GraphDatabaseService embedGraphDb(String graphFile){
         logger.debug("Creating database ...");
-
-        //graphFile is optional - we'll just create one if it doesn't exist
-        if(graphFile == null){
-            graphFile = "graph.db";
-        }
 
         GraphDatabaseService graphdb = new GraphDatabaseFactory().newEmbeddedDatabase(graphFile);
         registerShutdownHook(graphdb);
-        this._graphdb = graphdb;
+
+        return graphdb;
     }
 
     /**
-     * this method will generate a barabase-albert preferential matching graph
-     * from either an existing graph or create a new one and attach the ba
-     * generative model to it
-     *
-     * @param int n - the number of nodes to attach to this graph
-     * @param int m - the max number of edges m we should make per node n
-     *
+     * async hook to shutdown the db file cleanly
      */
-    public void generateBarabasiAlbert(int n, int m){
-        /*
-         * first node should be labeled root, so we know where
-         * we started
-         */
-        boolean firstNode = true;
-        Transaction tx = null;
-        try{
-            int batch = 10000;
-            tx = this._graphdb.beginTx();
-
-            //if graph is empty - need seeded graph
-            //@reference http://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model Algorithm section
-            if(this._isEmptyGraph()){
-                this._startGeneration();
-            }
-
-            int i = 0; //counter for batching - you'll thank me later
-            while(i <= n){
-                logger.info("Creating node " + i);
-                if(++i % batch == 0){
-                    tx.success();
-                    tx.close();
-                    tx = this._graphdb.beginTx();
-                }
-
-                //attach edges from node - number of edges determined randomly between 1 and m
-                int numberOfEdges = this._getRandomNumberOfEdgesFor(m);
-                logger.debug("Number of Edges: " + numberOfEdges);
-
-                //attach node n to m edges
-                logger.debug("Attaching node to random node(s) according to random edge value ...");
-                this._attachNodeWithEdges(numberOfEdges);
-            }
-
-            tx.success();
-            tx.close();
-        } catch (Exception ex){
-            logger.debug("ERR");
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            ex.printStackTrace(pw);
-            logger.debug(sw.toString());
-            tx.close();
-        }
-    }
-
-    /**
-     * choose a random node i - random between 0 and number of nodes
-     * find degree of chosen node i
-     * calc probability of attachment where probability = degree of node / nodes.size() * 2
-     * if attach probability >= next random double - attach else choose new node
-     * 
-     * @reference http://en.wikipedia.org/wiki/Barab%C3%A1si%E2%80%93Albert_model
-     */
-    private void _attachNodeWithEdges(int numberOfEdges){
-        Random random = new Random();
-        /*
-         * we want to make sure we don't use the possibly newly attached node edge
-         */
-        int sumDegree = this._sumDegreeOfGraph();
-
-        /*
-         * create node
-         */
-        String nodeName = String.format("child");
-        Node node = this._createNode(nodeName);
-        for(int i = 0; i < numberOfEdges; i++){
-            boolean createEdge = false;
-
-            Node randomNode = null;
-            do{
-                //choose random node
-                randomNode = this._chooseRandomNode(random);
-                logger.debug("Random node chosen ..");
-
-                int nodeDegree = randomNode.getDegree();
-                logger.debug("Random node has nodeDegree " + nodeDegree);
-
-                //calc probability of attachment
-                logger.debug(nodeDegree + " / " + sumDegree);
-                double attachProbability = ((nodeDegree + 0.0) / (sumDegree + 0.0));
-                logger.debug("Random node has attachProbability of " + attachProbability);
-
-                //if can attach, attach - otherwise we are trying again
-                double nextRandomDouble = random.nextDouble();
-                logger.debug("Next random double:  " + nextRandomDouble);
-                if(attachProbability >= nextRandomDouble){
-                    createEdge = true;
-                    logger.debug("Attaching node to chosen random node ...");
-                }
-            } while(!createEdge);
-
-
-            //create edge relationship between nodes
-            node.createRelationshipTo(randomNode, RelTypes.CONNECTED);
-            logger.debug("Relationship connected from node to random node ...");
-        }
-    }
-
-    /**
-     * convenience method to choose a random node
-     */
-    private Node _chooseRandomNode(Random random){
-        GlobalGraphOperations graphops = GlobalGraphOperations.at(this._graphdb);
-        Iterable<Node> nodes = graphops.getAllNodes();
-        List<Node> nodesList = IteratorUtil.asList(nodes);
-
-        return nodesList.get(random.nextInt(nodesList.size()));
-    }
-
-    /**
-     * get the degree sum of the graph
-     */
-    private int _sumDegreeOfGraph(){
-        GlobalGraphOperations graphops = GlobalGraphOperations.at(this._graphdb);
-        Iterable<Node> nodes = graphops.getAllNodes();
-
-        return IteratorUtil.count(nodes) * 2;
-    }
-
-    /**
-     * generate a random number between 1 and m
-     * @reference http://stackoverflow.com/questions/363681/generating-random-integers-in-a-range-with-java
-     * @param m - max value
-     * @return int
-     */
-    private int _getRandomNumberOfEdgesFor(int m){
-        Random random = new Random();
-        return random.nextInt((m - 1) + 1) + 1;
-    }
-
-
-    /**
-     * convenience method to create our new graph
-     * since we must not have an existing graph to play with
-     */
-    private void _startGeneration(){
-        Node rootNode = this._createNode("root");
-        Node childNode = this._createNode("child");
-        rootNode.createRelationshipTo(childNode, RelTypes.CONNECTED);
-    }
-
-    /**
-     * convenience method to create a node
-     */
-    private Node _createNode(String lbl){
-        Node node = this._graphdb.createNode();
-        Label label = DynamicLabel.label(lbl);
-        node.addLabel(label);
-
-        return node;
-    }
-
-    /**
-     * check if our graph is empty
-     * empty means 0 nodes
-     */
-    private boolean _isEmptyGraph(){
-        GlobalGraphOperations graphops = GlobalGraphOperations.at(this._graphdb);
-        Iterable<Node> nodes = graphops.getAllNodes();
-        if(IteratorUtil.count(nodes) > 0){
-            logger.debug("Existing graph ...");
-            return false;
-        }
-
-        logger.debug("New graph ...");
-        return true;
-    }
-
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
     {
         // Registers a shutdown hook for the Neo4j instance so that it
